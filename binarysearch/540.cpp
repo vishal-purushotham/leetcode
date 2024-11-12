8472791d40aeb96c21abb0103b7e21827881ec60
VishalP <buvan5894@gmail.com>
2024-11-12 19:36:10 +0530

    binsearch

---
+++ binarysearch/540.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int low=0,high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2==1 && nums[mid]==nums[mid-1])){
+                low=mid+1;a
+            }
+            else{high=mid;}
+        }
+        return nums[low];
+    }
+};
