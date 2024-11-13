4cda78f5b5620d7780b9cbd43161a530cc387891
VishalP <buvan5894@gmail.com>
2024-11-13 18:29:24 +0530

    binsearch

---
+++ binarysearch/countfairpairs.cpp
@@ -0,0 +1,12 @@
+long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n=nums.size();
+        long ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i++){
+            int x=lower-nums[i],y=upper-nums[i];
+            auto it=lower_bound(nums.begin()+i+1,nums.end(),x);
+            auto gt=upper_bound(nums.begin()+i+1,nums.end(),y);
+            ans+=gt-it;
+        }
+        return ans;
+    }
