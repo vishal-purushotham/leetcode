49285df17733c292d6c66bee5db28c35e95e876f
VishalP <buvan5894@gmail.com>
2024-11-18 10:25:56 +0530

    1652

---
+++ arrays/1652_defusebomb.cpp
@@ -0,0 +1,25 @@
+ vector<int> decrypt(vector<int>& code, int k) {
+        int n=code.size();
+        vector <int> temp;
+        if (k<0){
+            for(int i=0;i<n ;i++){
+            int sum=0;
+            for(int j=1;j<=abs(k);j++){
+                sum+=code[(i-j+n)%n];
+            }
+            temp.push_back(sum);
+        }
+        }
+        if(k>0 || k==0){
+        for(int i=0;i<n ;i++){
+            int sum=0;
+            for(int j=1;j<=k;j++){
+                sum+=code[(i+j)%n];
+            }
+            temp.push_back(sum);
+        
+        }
+        }
+        return temp;
+
+    }
