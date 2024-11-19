c190f51e3f884bbd1a257c2e3d55cf13f0290941
VishalP <buvan5894@gmail.com>
2024-11-19 17:56:15 +0530

    modified:   arrays/1652_defusebomb.cpp

--- arrays/1652_defusebomb.cpp
+++ arrays/1652_defusebomb.cpp
@@ -3,12 +3,12 @@
         vector <int> temp;
         if (k<0){
             for(int i=0;i<n ;i++){
-            int sum=0;
-            for(int j=1;j<=abs(k);j++){
-                sum+=code[(i-j+n)%n];
+                int sum=0;
+                for(int j=1;j<=abs(k);j++){
+                    sum+=code[(i-j+n)%n];
+                }
+                temp.push_back(sum);
             }
-            temp.push_back(sum);
-        }
         }
         if(k>0 || k==0){
         for(int i=0;i<n ;i++){
@@ -17,9 +17,7 @@
                 sum+=code[(i+j)%n];
             }
             temp.push_back(sum);
-        
         }
         }
         return temp;
-
     }
