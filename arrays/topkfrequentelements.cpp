f1473f8ae023e13604970cf98eb370be7440020e
VishalP <buvan5894@gmail.com>
2024-11-20 19:57:18 +0530

    arrays

--- arrays/topkfrequentelements.cpp
+++ arrays/topkfrequentelements.cpp
@@ -1,19 +1,16 @@
-vector<int> topKFrequent(vector<int>& nums, int k) {
+vector<int> topKFrequent(vector<int>& nums,int k) {
         map <int,int> mp;
         vector <int> ans;
         for(int x:nums){
             mp[x]++;
-         }
-     
+        }
         priority_queue <pair<int,int>> pq;
         for(auto& y:mp){
             pq.push({y.second,y.first});
-            
-         }
+        }
         for(int i=0;i<k;i++){
             ans.push_back(pq.top().second);
             pq.pop();
-            
-         }
+        }
         return ans;
     }
